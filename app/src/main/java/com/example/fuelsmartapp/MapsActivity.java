1401795226888ded40860d530d8095b37d3c133a
amanda <haydawam@deakin.edi.au>
2022-04-12 08:57:47 +1000

    new changes were made for main page , add vehicle expense

--- app/src/main/java/com/example/fuelsmartapp/MapsActivity.java
+++ app/src/main/java/com/example/fuelsmartapp/MapsActivity.java
@@ -1,21 +1,35 @@
 package com.example.fuelsmartapp;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.fuelsmartapp.databinding.ActivityMapsBinding;
-
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Button btn;
+    private Marker marker;
     private ActivityMapsBinding binding;
 
     @Override
@@ -25,6 +39,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+//        btn = findViewById(R.id.btn);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                marker.hideInfoWindow();
+//            }
+//        });
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -46,8 +67,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng Melb = new LatLng(-37.5971198,144.9037784);
-        mMap.addMarker(new MarkerOptions().position(Melb).title("7 Eleven"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(Melb));
+        mMap.addMarker(new MarkerOptions().position(Melb).title("7 Eleven").icon(bitmapDescriptorFromVector(getApplicationContext(),R.drawable.seveneleven)));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(Melb,15f));
+        // Setting a custom info window adapter for the google map
+//        InfoWindowAdapter markerInfoWindowAdapter = new InfoWindowAdapter(getApplicationContext());
+//        googleMap.setInfoWindowAdapter(markerInfoWindowAdapter);
+
+        // Adding and showing marker when the map is touched
+
+//        mMap.clear();
+//        MarkerOptions markerOptions = new MarkerOptions();
+//        markerOptions.position(Melb);
+//        mMap.animateCamera(CameraUpdateFactory.newLatLng(Melb));
+//        marker = mMap.addMarker(markerOptions);
+//         marker.showInfoWindow();
+
+//        googleMap.setOnInfoWindowClickListener(this::onInfoWindowClick);
+
     }
+
+    public void onInfoWindowClick(Marker marker) {
+        Toast.makeText(this, "Gir Forest Clicked!!!!", Toast.LENGTH_SHORT).show();
+    }
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
 }
