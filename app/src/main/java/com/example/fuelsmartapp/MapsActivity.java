ae281ee4c1a514fab3d2999d109a0287c1395a30
amanda <haydawam@deakin.edi.au>
2022-05-13 11:04:27 +1000

    enable GPS alert modify app icon on google store to have accurate image

--- app/src/main/java/com/example/fuelsmartapp/MapsActivity.java
+++ app/src/main/java/com/example/fuelsmartapp/MapsActivity.java
@@ -1,12 +1,19 @@
 package com.example.fuelsmartapp;
 
+import static com.example.fuelsmartapp.signup.TAG;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,11 +21,15 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.SearchView;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.example.fuelsmartapp.databinding.ActivityMapsBinding;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -38,8 +49,11 @@ import java.nio.charset.Charset;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+    private static final int PERMISSIONS_REQUEST_ENABLE_GPS = 9002;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9003;
     private GoogleMap mMap;
+    private boolean mLocationPermissionGranted = false;
     GoogleApiClient mGoogleApiClient;
     private Button btn;
     private Marker marker;
@@ -52,6 +66,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
@@ -67,6 +82,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //                marker.hideInfoWindow();
 //            }
 //        });
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -107,16 +124,130 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
+    private boolean checkMapServices() {
+        if (isServicesOK()) {
+            if (isMapsEnabled()) {
+                System.out.println("reach1");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("GPS Required. Enable GPS otherwise location tracking won’t work! ")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        Intent enableGpsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivityForResult(enableGpsIntent, PERMISSIONS_REQUEST_ENABLE_GPS);
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public boolean isMapsEnabled() {
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+            buildAlertMessageNoGps();
+            return false;
+        } else {
+            System.out.println("reach2");
+
+            readDataFromCSV();
+        }
+        return true;
+
+    }
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+//            getChatrooms();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    public boolean isServicesOK() {
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MapsActivity.this);
+
+        if (available == ConnectionResult.SUCCESS) {
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MapsActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        mLocationPermissionGranted = false;
+
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: called.");
+        if (mLocationPermissionGranted) {
+            Log.d(TAG, "readDataFromCSV: called.");
+            readDataFromCSV();
+        } else {
+            getLocationPermission();
+        }
+//        switch (requestCode) {
+//            case PERMISSIONS_REQUEST_ENABLE_GPS: {
+//                if (mLocationPermissionGranted) {
+//                    Log.d(TAG, "readDataFromCSV: called.");
+//                    readDataFromCSV();
+//                } else {
+//                    getLocationPermission();
+//                }
+//            }
+//        }
+
+    }
+
     public void passData() {
         TextView display = findViewById(R.id.fuel_type_text);
         Bundle bn = getIntent().getExtras();
         String name = bn.getString("text5");
         display.setText(String.valueOf(name));
     }
-
-
-
-
 
 
     /**
@@ -131,6 +262,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
 
         googleMap.getUiSettings().setZoomControlsEnabled(true);
@@ -140,9 +272,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
         googleMap.setOnInfoWindowClickListener(this::onInfoWindowClick);
-        readDataFromCSV();
-    }
 
+        checkMapServices();
+
+    }
 
 
     private void readDataFromCSV() {
@@ -194,7 +327,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-
     public void adding_vehicle_expenses(View view) {
 //        Intent intend = new Intent(this, adding_vehicle_expense.class);
 //        startActivity(intend);
@@ -211,6 +343,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Toast.makeText(this, "Service is not available !", Toast.LENGTH_LONG).show();
 
     }
+
     public void notification(View view) {
         Toast.makeText(this, "Service is not available !", Toast.LENGTH_LONG).show();
     }
